cfe8633a788300568d6adf47deac8534a0b7e564
git112 <23it021@charusat.edu.in>
2025-01-22 16:13:35 +0530

    Header & Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'; // Renamed for clarity (optional)
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Header />
+      </>
+   
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from './ui/button'
+
+function Header() {
+  return (
+    
+    <section className="bg-gray-900 text-white">
+  <div className="mx-auto max-w-screen-xl px-4 lg:flex lg:h-screen lg:items-center">
+    <div className="mx-auto max-w-5xl text-center">
+      <h1
+        className="text-blue-200 font-bold sm:text-7xl"
+      >
+        All Your File Conversions,
+
+        <span className="sm:block"> Just a Click Away! </span>
+      </h1>
+
+      <p className="
+      bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent
+      mx-auto mt-6 max-w-2xl sm:text-2xl/relaxed">
+        One Platform, Endless Possibilities - Start Converting Now!
+      </p>
+      <div className="mt-8">
+      <Button/>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+
+  )
+}
+
+export default Header
